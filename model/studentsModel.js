534cfbbab4af82f202adffedbf921c1a64127d71
lingfeng <2396847478@qq.com>
2022-01-30 23:57:56 +0800

    mongoose模块化模块

---
+++ model/studentsModel.js
@@ -0,0 +1,40 @@
+let mongoose=require('mongoose')
+//1.引入模式对象
+let Schema = mongoose.Schema
+
+//2.创建约束对象
+let studentsRule = new Schema({
+  stu_id:{
+    type:String, //限制学号必须为：字符串
+    required:true,
+    unique:true
+  },
+  name:{
+    type:String, //限制姓名必须为：字符串
+    required:true, //限制姓名为必填项
+  },
+  age:{
+    type:Number, //限制年龄必须为：字符串
+    required:true, //限制年龄为必填项
+  },
+  sex:{
+    type:String, //限制性别必须为：字符串
+    required:true, //限制性别为必填项
+  },
+  info:Schema.Types.Mixed, //接收所有类型
+  date:{
+    type:Date,
+    default:Date.now()
+  },
+  price:{
+    type:String, //限制工资必须为：字符串
+    required:true, //限制工资为必填项
+  },
+  enable_flag:{
+    type:String,
+    default:'Y'
+  }
+})
+
+//3.创建模型对象
+module.exports = mongoose.model('students',studentsRule) //用于生成某个集合所对应的模型对象
---
+++ mongooseCRUD.js
@@ -0,0 +1,26 @@
+
+//1. 引入第三方库
+let mongoose=require('mongoose');
+//2.引入数据库连接模块
+let db      =require('./db/db');
+//3.引入模型对象
+let stuModel=require('./model/studentsModel')
+
+
+db(function(err){
+    if(err){console.log(err)}
+    else{
+        //4.判断数据库连接状态.成功CRUD,错误报告
+        //console.log('操作数据库的代码');
+        stuModel.create({//stuModel 可改
+          stu_id:'12',
+          age:100,
+          sex:'男',
+          price:'11.5k',
+          name:'XiXi'
+          },function(err,data){
+            if(!err){console.log(data)}
+            else console.log(err)
+          })
+    }
+})
